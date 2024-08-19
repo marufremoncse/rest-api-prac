89fab77b1da7fed967ef4628a1147618db04b61b
marufremoncse <marufremon.cse14.2@gmail.com>
2024-08-19 18:48:23 +0600

    add target

---
+++ target/classes/data.sql
@@ -0,0 +1,18 @@
+insert into user(id, name, birthDate)
+values(1, 'Remon', current_date());
+
+
+insert into user(id, name, birthDate)
+values(2, 'Tomon', current_date());
+
+insert into post(id, description, user_id)
+values(1, 'Learn Spring boot', 1);
+
+insert into post(id, description, user_id)
+values(2, 'Learn Marketing', 2);
+
+insert into post(id, description, user_id)
+values(3, 'Learn AWS', 1);
+
+insert into post(id, description, user_id)
+values(4, 'Learn Accounting', 2);
